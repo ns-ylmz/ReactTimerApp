2125ce921fb5f8f76165b4629c7f79fc505f4dc4
Enes YILMAZ <enisyilmaz12@gmail.com>
2017-02-05 04:04:08 +0300

    Create clock component

--- app/app.jsx
+++ app/app.jsx
@@ -17,7 +17,7 @@ ReactDOM.render(
 	<Router history={hashHistory}>
 		<Route path="/" component={Main}>
 			<Route path="countdown" component={Countdown}/>
-			<IndexRoute path="timer" component={Timer}/>
+			<IndexRoute component={Timer}/>
 		</Route>
 	</Router>,
 	document.getElementById('app')
